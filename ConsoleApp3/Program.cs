b400aac5769adf45fe2211b177c78b522fbf49da
Tommaso Castagnoli <tommasocastagnoli29@gmail.com>
2024-02-13 15:54:31 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Vector v = new Vector(7, 14);
+            Vector vv = new Vector(69,4);
+
+            Vector r = new Vector(0,0);
+
+            r = v + vv;
+
+            Console.WriteLine("{0}",r.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp3/Vector.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Vector
+    {
+        private readonly float _x;  
+        private readonly float _y;
+
+        public float X { get { return _x; } }
+        public float Y { get { return _y;} }
+
+        public Vector(float x, float y)
+        {
+            _x = x;
+            _y = y;
+        }
+        public static Vector operator+(Vector a, Vector b) {
+            Vector v= new Vector(a.X+b.X,a.Y+b.Y);
+            return v;
+        }
+
+        public override string ToString()
+        {
+            return $"{X};{Y}";
+        }
+
+        public static Vector operator - (Vector effe)
+        {
+            Vector f = new Vector(effe.Y, effe.X);
+            return f;
+        }
+
+        public static Vector operator -(Vector a, Vector b)
+        {
+            Vector emme = new Vector(a.X - b.X, a.Y - b.Y);
+            return emme;
+        }
+
+        public static double operator *(Vector a, Vector b)
+        {
+            double risultato = a * b * Math.Cos(35);
+            return risultato;
+        }
+
+        public static  Vector Parse(string s)
+        {
+           var parts= s.Split(';');
+            Vector sa = new Vector(int.Parse(parts[0]), int.Parse(parts[1]));
+
+            return sa;
+        }
+
+        public static bool TryParse(string saa, out Vector veee)
+        {
+            try
+            {
+                veee = Vector.Parse(saa);
+                return true;
+            }
+            catch
+            {
+                veee = null;
+                return false;   
+            }
+        }
+
+    }
+}
